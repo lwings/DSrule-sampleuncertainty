2f42fad3be12f932ae746f81261a0e9a38075362
little_wings <little_wings@foxmail.com>
2019-03-11 16:46:53 +0800

    20190311

--- CT-NN/NNAPI.py
+++ CT-NN/NNAPI.py
@@ -4,26 +4,14 @@ import tensorflow as tf
 import numpy as np
 
 
-data_dic = get_datadic()
-
-trainVector = []
-trainLabel = []
-testSeq = []
-testVector = []
-testLabel = []
-
-for k,v in data_dic.items() :
-    if k<3000:
-        trainVector.append(v[0:12])
-        trainLabel.append(v[-2])
-    else:
-        testVector.append(v[0:12])
-        testLabel.append(v[-2])
-        testSeq.append(k)
-
-x_data = np.array(trainVector)
-y_data = np.array(trainLabel).reshape(-1,1)
-
+def accuracy(pvalue,tvalue):
+    trueCase=0
+    for i in range(0,len(tvalue)):
+        idx1 = pvalue[i].index(max(pvalue[i]))
+        idx2 = tvalue[i].index(1)
+        if(idx1==idx2):
+            trueCase+=1
+    return trueCase*1.0/len(tvalue)
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
 # add one more layer and return the output of this layer
@@ -37,33 +25,86 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
     return outputs
     # Make up some real data
 
+data_dic = get_datadic()
+
+trainVector = []
+trainLabel = []
+testSeq = []
+testVector = []
+testLabel = []
+
+counter={}
+tempLabel = [v[-2] for k,v in data_dic.items()]
+for i in tempLabel:
+    counter[i] = counter.get(i,0) + 1
+reflection = {}
+idx=0
+for k,v in counter.items():
+    reflection[k]=idx
+    idx+=1
+
+for k,v in data_dic.items() :
+    temp =[0]*8
+    temp[reflection[v[-2]]] = 1
+    if k<3000:
+        trainVector.append(v[0:12])
+        trainLabel.append(temp)
+    else:
+        testVector.append(v[0:12])
+        testLabel.append(temp)
+        testSeq.append(k)
+
+
+x_data = np.array(trainVector)
+y_data = np.array(trainLabel)
+x_test_data = np.array(testVector)
+y_test_data = np.array(testLabel)
+
+
+
 # define placeholder for inputs to network
 xs = tf.placeholder(tf.float32, [None, 12])
-ys = tf.placeholder(tf.float32, [None, 1])
-# add hidden layer
-l1 = add_layer(xs, 12, 10, activation_function=tf.nn.relu)
+ys = tf.placeholder(tf.float32, [None, 8])
 # add output layer
-prediction = add_layer(l1, 10, 1, activation_function=None)
+prediction = add_layer(xs, 12, 8,  activation_function=tf.nn.softmax)
+
 # the error between prediction and real data
-loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
-reduction_indices=[1]))
-train_step = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),
+                                              reduction_indices=[1]))       # loss
+train_step = tf.train.GradientDescentOptimizer(0.03).minimize(cross_entropy)
+
+sess = tf.Session()
 # important step
 # tf.initialize_all_variables() no long valid from
 # 2017-03-02 if using tensorflow >= 0.12
-if int((tf.__version__).split('.')[1]) < 12:
+if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
     init = tf.initialize_all_variables()
 else:
     init = tf.global_variables_initializer()
-sess = tf.Session()
 sess.run(init)
-for i in range(1000):
-# training
+
+for i in range(5000):
     sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
     if i % 50 == 0:
-    # to see the step improvement
-        print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
-        
-        
- test_data = np.array(testVector)       
- prediction_value = sess.run(prediction, feed_dict={xs: test_data})
+        prediction_value = sess.run(prediction, feed_dict={xs: x_data})
+        print(accuracy(prediction_value.tolist(),trainLabel))
+
+prediction_value = sess.run(prediction, feed_dict={xs: x_test_data})
+
+
+print(accuracy(prediction_value.tolist(),testLabel))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
